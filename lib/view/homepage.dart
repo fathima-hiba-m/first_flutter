aaa41ec5c38f859adc9bec0fc81c4b644b801520
Fathima Hiba M <fathimahibam3@email.com>
2026-01-18 01:07:18 +0530

    changed message to hi this is my app

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -13,7 +13,7 @@ class Homepage extends StatelessWidget {
     final controller = Get.put(DataController());
     return Scaffold(
       appBar: AppBar(
-        title: const Text("message"),
+        title: const Text("Hi this is my first app"),
       ),
       body: Container(
         alignment: Alignment.center,
